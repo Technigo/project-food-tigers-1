78ebb58de1c9ecd3760ab605fb1d4a2dddbd07e4
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-29 16:42:17 +0200

    Got all data console logged in JS, ready for HTML

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@ const API = "967cabdca4d45ad5a19691662be081c0";
 const city = 61; // London
 const cuisine = 60; // Japanese
 
-const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${city}&cuisines=${cuisine}`;
+const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${city}&entity_type=city&cuisines=${cuisine}`
 
 fetch(apiURL, {
     headers: { "user-key": API }
@@ -13,13 +13,19 @@ fetch(apiURL, {
     .then((json) => {
         console.log(json)
 
-
-
         json.restaurants.forEach((resto) => {
             console.log(resto.restaurant);
             const name = resto.restaurant.name;
+            const price = resto.restaurant.average_cost_for_two;
+            const location = resto.restaurant.location;
+            const raiting = resto.restaurant.user_rating.aggregate_rating;
+            const img = resto.restaurant.featured_image;
+            console.log(img)
+            console.log(raiting)
+            console.log(location)
             console.log(name)
-            document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.name}`
+            console.log(price)
+            document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.name}</li>`
 
         });
     });
