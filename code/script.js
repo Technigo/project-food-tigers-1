7c59a137d3d37f0ff64b6ea8f1e0c51cb691e8ea
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-30 13:48:52 +0200

    final commit

--- code/script.js
+++ code/script.js
@@ -2,21 +2,11 @@ const API = "967cabdca4d45ad5a19691662be081c0"
 const city = 61 // London
 const cuisine = 60 // Japanese
 
+
 const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${city}&entity_type=city&cuisines=${cuisine}`
 
 const restaurants = []
 const main = document.querySelector('main')
-
-const priceInfo = () => {
-    main.innerHTML = ""
-    restaurants.filter(resto => {
-        if (resto.valueRange) {
-            main.innerHTML += `<div class="pictures"><img src=${resto.restaurant.featured_image}/>
-        <h3>${resto.name}</h3></div>`
-        }
-    })
-
-}
 
 fetch(apiURL, { headers: { "user-key": API } })
     .then((response) => {
@@ -25,7 +15,7 @@ fetch(apiURL, { headers: { "user-key": API } })
     .then((json) => {
         json.restaurants.forEach(resto => {
             const value = resto.restaurant.price_range
-            const type = resto.restaurant.establishment
+            const dollar = resto.restaurant.price_range
 
             let range = resto.restaurant.price_range
             if (range <= 2) {
@@ -45,10 +35,12 @@ fetch(apiURL, { headers: { "user-key": API } })
                 location: resto.restaurant.location.address,
                 valueRange: range,
                 value,
-                type
+                dollar
+
             })
         })
         console.log(restaurants)
+
 
         json.restaurants.forEach((resto) => {
             console.log(resto.restaurant)
@@ -57,17 +49,7 @@ fetch(apiURL, { headers: { "user-key": API } })
             const location = resto.restaurant.location.address
             const raiting = resto.restaurant.user_rating.aggregate_rating
             const img = resto.restaurant.thumb
-            console.log(img)
-            console.log(raiting)
-            console.log(location)
-            console.log(name)
-            console.log(price)
-
-            document.getElementById("restaurants").innerHTML += `<ol><img src=${img}/>${name}${price}${location}${raiting}</ol>`
+            document.getElementById("restaurants").innerHTML += `<div class="restaurant-container"><img class="dishes-img" src=${img}><h2 class"rest-name">${name}</h2><span class="rating">${raiting}/5 ⭐️</span><span class="price">Averege price: ${price / 2}💲</span><span class="location">📍${location}</span> </div> `
 
         })
     })
-
-
-
-
