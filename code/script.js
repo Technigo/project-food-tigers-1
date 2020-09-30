32198397ff79cb9c76e6f79465f0ccb8ed963866
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-30 08:47:08 +0200

    Nothing showing up on api branch

--- code/script.js
+++ code/script.js
@@ -1,25 +1,23 @@
-const API = "967cabdca4d45ad5a19691662be081c0";
-const city = 61; // London
-const cuisine = 60; // Japanese
+const API = "967cabdca4d45ad5a19691662be081c0"
+const city = 61 // London
+const cuisine = 60 // Japanese
 
 const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${city}&entity_type=city&cuisines=${cuisine}`
 
-fetch(apiURL, {
-    headers: { "user-key": API }
-})
+fetch(apiURL, { headers: { "user-key": API } })
     .then((response) => {
-        return response.json();
+        return response.json()
     })
     .then((json) => {
         console.log(json)
 
         json.restaurants.forEach((resto) => {
-            console.log(resto.restaurant);
-            const name = resto.restaurant.name;
-            const price = resto.restaurant.average_cost_for_two;
-            const location = resto.restaurant.location;
-            const raiting = resto.restaurant.user_rating.aggregate_rating;
-            const img = resto.restaurant.featured_image;
+            console.log(resto.restaurant)
+            const name = resto.restaurant.name
+            const price = resto.restaurant.average_cost_for_two
+            const location = resto.restaurant.location
+            const raiting = resto.restaurant.user_rating.aggregate_rating
+            const img = resto.restaurant.featured_image
             console.log(img)
             console.log(raiting)
             console.log(location)
@@ -27,7 +25,7 @@ fetch(apiURL, {
             console.log(price)
             document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.name}</li>`
 
-        });
-    });
+        })
+    })
 
 
